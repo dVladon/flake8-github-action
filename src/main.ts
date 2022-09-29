62ab6e4b151c434a3c29c204d98c5903378e0289
dVladon <vladislav.vlasov.dev@gmail.com>
2022-09-29 10:04:50 +0200

    Add running flake8

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,55 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 import * as github from '@actions/github';
 
+async function runCheck(pathToCheck: string) {
+    let out = "";
+    let opts = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                out += data.toString();
+            },
+        },
+    };
+
+    await exec.exec(`flake8 --exit-zero --max-line-length=120 ${pathToCheck}`, [], opts);
+
+    return out;
+}
+
 async function run() {
-    try {
-        const token = core.getInput('gh_token');
-        const octokit = github.getOctokit(token);
-        
-        const sha = core.getInput('commit_sha');
-        console.log(sha);
+    const token = core.getInput('gh_token');
+    const sha = core.getInput('commit_sha');
+    const checkPath = core.getInput('check_path');
 
-        const check = await octokit.rest.checks.create({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            name: "Flake8 Check Result",
-            head_sha: sha,
-            status: "completed",
-            conclusion: "success",
-            output: {
-                title: "Flake8 Check Detailed Report",
-                summary: "",
-                annotations: [
-                    {
-                        path: ".github/workflows/flake8.yml",
-                        start_line: 1,
-                        end_line: 1,
-                        annotation_level: "failure",
-                        message: "[E000] Test error",
-                        start_column: 1,
-                        end_column: 1
-                    }
-                ]
-            }
-        });
+    const octokit = github.getOctokit(token);
 
-        console.log(check)
-    } catch (error) {
-        // if (error instanceof Error) {
-        //     core.setFailed(error.message);
-        // }
-        throw error;
-    }
+    const out = runCheck(checkPath);
+    console.log(out);
+
+    // const check = await octokit.rest.checks.create({
+    //     owner: github.context.repo.owner,
+    //     repo: github.context.repo.repo,
+    //     name: "Flake8 Check Result",
+    //     head_sha: sha,
+    //     status: "completed",
+    //     conclusion: "success",
+    //     output: {
+    //         title: "Flake8 Check Detailed Report",
+    //         summary: "",
+    //         annotations: [
+    //             {
+    //                 path: ".github/workflows/flake8.yml",
+    //                 start_line: 1,
+    //                 end_line: 1,
+    //                 annotation_level: "failure",
+    //                 message: "[E000] Test error",
+    //                 start_column: 1,
+    //                 end_column: 1
+    //             }
+    //         ]
+    //     }
+    // });
 }
 
 run();

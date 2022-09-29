53dc78a317a3340c6da9ffd404407eac3a7252fe
dVladon <vladislav.vlasov.dev@gmail.com>
2022-09-29 10:32:13 +0200

    Add check run logic

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as github from '@actions/github';
 
+type Flake8Error = {
+    path: string,
+    start_line: number,
+    end_line: number,
+    start_column: number, 
+    end_column: number,
+    annotation_level: string,
+    message: string,
+}
+
 async function runCheck(pathToCheck: string) {
     let out = "";
     let opts = {
@@ -17,6 +27,29 @@ async function runCheck(pathToCheck: string) {
     return out;
 }
 
+async function parseCheckOutput(raw_output: string): Promise<Flake8Error[]> {
+    const reg = new RegExp('/^(.*py):(\d+):(\d+):\s(\w\d+)\s(.*)$/');
+    
+    let errors: Flake8Error[] = [];
+
+    raw_output.split('\n').forEach((e) => {
+        let match = e.match(reg);
+        if (match) {
+            errors.push({
+                path: match[1].replace('./', ''),
+                start_line: parseInt(match[2]),
+                end_line: parseInt(match[2]),
+                start_column: parseInt(match[3]),
+                end_column: parseInt(match[3]),
+                annotation_level: "failure",
+                message: `[${match[4]}] ${match[5]}`,
+            });
+        }
+    });
+
+    return errors;
+}
+
 async function run() {
     const token = core.getInput('gh_token');
     const sha = core.getInput('commit_sha');
@@ -24,32 +57,38 @@ async function run() {
 
     const octokit = github.getOctokit(token);
 
-    const out = runCheck(checkPath);
-    console.log(out);
+    let out = await runCheck(checkPath);
+    let errors = await parseCheckOutput(out);
 
-    // const check = await octokit.rest.checks.create({
-    //     owner: github.context.repo.owner,
-    //     repo: github.context.repo.repo,
-    //     name: "Flake8 Check Result",
-    //     head_sha: sha,
-    //     status: "completed",
-    //     conclusion: "success",
-    //     output: {
-    //         title: "Flake8 Check Detailed Report",
-    //         summary: "",
-    //         annotations: [
-    //             {
-    //                 path: ".github/workflows/flake8.yml",
-    //                 start_line: 1,
-    //                 end_line: 1,
-    //                 annotation_level: "failure",
-    //                 message: "[E000] Test error",
-    //                 start_column: 1,
-    //                 end_column: 1
-    //             }
-    //         ]
-    //     }
-    // });
+    if (errors.length == 0) {
+        await octokit.rest.checks.create({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            name: "Flake8 Report",
+            head_sha: sha,
+            status: "completed",
+            conclusion: "success",
+            output: {
+                title: "Flake8 Check Results",
+                summary: "No issues found!",
+                annotations: []
+            }
+        });
+    } else {
+        await octokit.rest.checks.create({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            name: "Flake8 Report",
+            head_sha: sha,
+            status: "completed",
+            conclusion: "failure",
+            output: {
+                title: "Flake8 Check Results",
+                summary: `Found ${errors.length} issues.`,
+                annotations: errors
+            }
+        });
+    }
 }
 
 run();

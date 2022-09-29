07cd3a1e51d0dbc3c5b6dd07b065424c296016c3
dVladon <vladislav.vlasov.dev@gmail.com>
2022-09-29 08:06:36 +0200

    Fix import s

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from "@actions/core"
+import * as github from "@actions/github"
 
 (async () => {
     try {
         core.notice('Started Flake8 Github Action.')
     } catch (error) {
-        core.setFailed(error);
+        core.setFailed('Error when executing action!');
     }
-});
+})();

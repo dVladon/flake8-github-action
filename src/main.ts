2b3d7daa106f603cf680bea178f04029bd4473a1
dVladon <vladislav.vlasov.dev@gmail.com>
2022-09-29 08:51:33 +0200

    Add code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { Octokit } from "octokit";
 
 async function run() {
     try {
         core.notice('Started Flake8 Github Action.');
+
+        const token = core.getInput('gh-token');
+        const octokit = new Octokit({ auth: String(token) });
+
+        const check = await octokit.rest.checks.listForRef({
+            check_name: github.context.job,
+            ...github.context.repo,
+            ref: github.context.sha
+          });
+
+        console.log(check)
     } catch (error) {
-        core.setFailed('Error hapenned');
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
     }
 }
 
